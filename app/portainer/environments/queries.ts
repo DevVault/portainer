d71585a6f09edcc3031bdc128e9c88353ec354a5
Richard Wei <degui.wei@gmail.com>
2022-04-14 11:25:10 +1200

    Homepage Filter Component

--- app/portainer/environments/queries.ts
+++ app/portainer/environments/queries.ts
@@ -4,19 +4,25 @@ import { getEndpoints } from '@/portainer/environments/environment.service';
 import { EnvironmentStatus } from '@/portainer/environments/types';
 import { error as notifyError } from '@/portainer/services/notifications';
 
+import type { EnvironmentType } from './types';
+
 const ENVIRONMENTS_POLLING_INTERVAL = 30000; // in ms
 
 export function useEnvironmentList(
   page: number,
   pageLimit: number,
   textFilter: string,
-  refetchOffline = false
+  refetchOffline = false,
+  environmentType?: EnvironmentType[]
 ) {
   const { isLoading, data } = useQuery(
-    ['environments', page, pageLimit, textFilter],
+    ['environments', page, pageLimit, textFilter, environmentType],
     async () => {
       const start = (page - 1) * pageLimit + 1;
-      return getEndpoints(start, pageLimit, { search: textFilter });
+      return getEndpoints(start, pageLimit, {
+        search: textFilter,
+        types: environmentType,
+      });
     },
     {
       keepPreviousData: true,
@@ -28,7 +34,6 @@ export function useEnvironmentList(
         const hasOfflineEnvironment = data.value.some(
           (env) => env.Status === EnvironmentStatus.Down
         );
-
         return hasOfflineEnvironment && ENVIRONMENTS_POLLING_INTERVAL;
       },
       onError(error) {
